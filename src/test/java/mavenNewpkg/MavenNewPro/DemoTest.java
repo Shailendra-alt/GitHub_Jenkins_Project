129ecdd77f72850e1157cf984d250ad164220bb3
Shailendra-alt <shailendkyadav190@gmail.com>
2020-09-10 17:12:51 +0530

    Thi is Maven GitHub Project

---
+++ src/test/java/mavenNewpkg/MavenNewPro/DemoTest.java
@@ -0,0 +1,21 @@
+package mavenNewpkg.MavenNewPro;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+		}
+	@Test
+	public void sub() {
+		System.out.println("SUB");
+		int a=30;
+		int b=20;
+		Assert.assertEquals(10, a-b);
+	}
+}
